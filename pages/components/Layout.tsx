9a2a6af2241da8d987a2e62b1c82b9564bafffb4
Kanta-Yamamoto <84559990+Kanta-Yamamoto@users.noreply.github.com>
2023-04-02 03:44:45 +0900

    サイドバーの実装

--- pages/components/Layout.tsx
+++ pages/components/Layout.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import { ReactNode } from "react";
+import { Sidebar } from "./Sidebar";
 
 interface LayoutProps {
   children: ReactNode;
@@ -7,21 +8,19 @@ interface LayoutProps {
 
 function Layout({ children }: LayoutProps) {
   return (
-    <div className="bg-bg-gray h-screen w-full text-black">
+    <div className="bg-bg-gray h-screen text-black w-full">
       <Head>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <header className="bg-white h-[60px] flex justify-between items-center px-4">
-        <div className="h-10 w-10 bg-gray rounded-4 p-2 flex flex-col justify-around">
-          <div className="rounded-full h-1 w-full bg-black" />
-          <div className="rounded-full h-1 w-full bg-black" />
-          <div className="rounded-full h-1 w-full bg-black" />
-        </div>
-        <div className="text-max font-extrabold h-max w-max">MyDev</div>
-      </header>
-      <main className="px-4 pt-8 sm:pt-10 sm:px-0 max-w-[400px] mx-auto flex justify-center items-center flex-col">
-        {children}
-      </main>
+
+      <div className="w-full h-[60px] bg-white">
+        <Sidebar
+          type="default"
+          avatarSrc="/default_avatar.svg"
+          name="デフォルトユーザー"
+        />
+      </div>
+      <main className="w-full">{children}</main>
     </div>
   );
 }
--- pages/mypage/index.tsx
+++ pages/mypage/index.tsx
@@ -7,19 +7,21 @@ export default function MyPage() {
   return (
     <>
       <Layout>
-        <div className="w-full flex flex-col items-center gap-4">
-          <div className="bg-white rounded-8 p-4 w-full">
-            {/* <span className=""> */}
-            <Avatar src="" alt="" size="large" className="mx-auto" />
-            {/* </span> */}
-            <p>名前：</p>
-            <p>好きなもの：</p>
-            <p>自己紹介：</p>
-            <p>タグ:</p>
+        <div className="px-4 pt-8 sm:pt-10 sm:px-0 max-w-[400px] mx-auto flex justify-center items-center flex-col">
+          <div className="w-full flex flex-col items-center gap-4">
+            <div className="bg-white rounded-8 p-4 w-full">
+              {/* <span className=""> */}
+              <Avatar src="" alt="" size="large" className="mx-auto" />
+              {/* </span> */}
+              <p>名前：</p>
+              <p>好きなもの：</p>
+              <p>自己紹介：</p>
+              <p>タグ:</p>
+            </div>
+            <Link href="/mypage/edit">
+              <Button color="gray">編集</Button>
+            </Link>
           </div>
-          <Link href="/mypage/edit">
-            <Button color="gray">編集</Button>
-          </Link>
         </div>
       </Layout>
     </>
