18330f456aa696714de962c2171317a6409d6833
Kanta-Yamamoto <84559990+Kanta-Yamamoto@users.noreply.github.com>
2023-04-02 14:23:33 +0900

    faviconやtitleロゴの設定

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Image from "next/image";
 import { ReactNode } from "react";
 import Sidebar from "./Sidebar";
 
@@ -13,15 +14,23 @@ function Layout({ children }: LayoutProps) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className="w-full h-[60px] bg-white fixed top- left-0">
+      <div className="w-full h-[60px] bg-main fixed top-0 left-0 z-10">
         <Sidebar
           type="default"
           avatarSrc="/default_avatar.svg"
           name="デフォルトユーザー"
         />
+
+        <Image
+          src="/quest-agora-title.svg"
+          alt=""
+          width={300}
+          height={60}
+          className="ml-auto mr-0"
+        />
       </div>
-      <div className="h-[60px] w-full"></div>
-      <main className="px-4 pt-8 sm:px-0 max-w-[400px] mx-auto flex justify-center items-center flex-col pb-10">
+
+      <main className="px-4 pt-20 sm:px-0 max-w-[400px] mx-auto flex justify-center items-center flex-col pb-10">
         {children}
       </main>
     </div>
